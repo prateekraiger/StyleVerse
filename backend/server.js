9d00905cb169701baf0131ccee82b093cc23c7be
Pratik <prateekraiger123@gmail.com>
2025-03-26 23:25:01 +0530

    added mongo

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
- 
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get("/", (req, res) => {
+  res.send("Testing APi");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
