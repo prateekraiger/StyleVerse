f56987699b60ddc94708e0eaaee44b8567b6a455
Pratik <prateekraiger123@gmail.com>
2025-03-27 23:28:13 +0530

    updated server.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,72 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+// user login
+
+const createToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET);
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    if (!user.password) {
+      return res.json({ success: false, message: "User not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Something went wrong" });
+  }
+};
+
+// register user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check if user already exists
+    const exits = await userModel.findOne({ email });
+    if (exits) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validation email and password
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Invalid email" });
+    }
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Password too short" });
+    }
+
+    //   hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // register user
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Something went wrong" });
+  }
+};
+
+// admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRoutes from "./routes/userRoutes.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
+connectCloudinary();
 
 // middleware
 app.use(express.json());
@@ -16,6 +19,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Testing APi");
 });
+
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
